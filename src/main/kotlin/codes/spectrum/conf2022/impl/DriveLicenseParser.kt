0ac2169d48ecf1164eacac414508cd2bd884437c
Ilya Pirozhkov <pirozhkov.i@avtocod.ru>
2022-11-10 17:44:59 +0700

    simple driverlicense and passport impl

--- src/main/kotlin/codes/spectrum/conf2022/impl/DriveLicenseParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/impl/DriveLicenseParser.kt
@@ -1,10 +1,19 @@
 package codes.spectrum.conf2022.impl
 
+import codes.spectrum.conf2022.doc_type.DocType
 import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.output.ExtractedDocument
 
-class DriveLicenseParser: IDocParser {
+class DriveLicenseParser : IDocParser {
     override fun parse(input: String): List<ExtractedDocument> {
-        TODO("Not yet implemented")
+        val prepared = input.filter { it.isDigit() }
+        return if (prepared.matches(DocType.DRIVER_LICENSE.normaliseRegex)) {
+            listOf(
+                ExtractedDocument(
+                    docType = DocType.DRIVER_LICENSE,
+                    value = prepared,
+                )
+            )
+        } else emptyList()
     }
 }
--- src/main/kotlin/codes/spectrum/conf2022/impl/PassportParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/impl/PassportParser.kt
@@ -1,10 +1,24 @@
 package codes.spectrum.conf2022.impl
 
+import codes.spectrum.conf2022.doc_type.DocType
 import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.output.ExtractedDocument
 
-class PassportParser: IDocParser {
+class PassportParser : IDocParser {
     override fun parse(input: String): List<ExtractedDocument> {
-        return emptyList()
+        val prepared = input.filter { it.isDigit() }
+        return if (prepared.matches(DocType.PASSPORT_RF.normaliseRegex)) {
+            val isValid = isRegionValid(prepared.take(2))
+            listOf(
+                ExtractedDocument(
+                    docType = DocType.PASSPORT_RF,
+                    value = prepared,
+                    isValidSetup = true,
+                    isValid = isValid,
+                )
+            )
+        } else emptyList()
     }
+
+    private fun isRegionValid(region: String): Boolean = region != "00"
 }
