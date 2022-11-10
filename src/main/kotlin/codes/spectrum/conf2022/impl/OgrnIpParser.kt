20304759b4252aac77fc5ed5b692325c54457b4c
nefedov.i <nefedov.i@spectrumdata.ru>
2022-11-10 16:26:36 +0500

    ogrn, ogrnip parser

--- src/main/kotlin/codes/spectrum/conf2022/impl/OgrnIpParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/impl/OgrnIpParser.kt
@@ -1,10 +1,26 @@
 package codes.spectrum.conf2022.impl
 
+import codes.spectrum.conf2022.doc_type.DocType
 import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.output.ExtractedDocument
 
 class OgrnIpParser: IDocParser {
+
     override fun parse(input: String): List<ExtractedDocument> {
-        TODO("Not yet implemented")
+        val onlyDigits = input.filter {it.isDigit() }
+        return if (onlyDigits.matches(DocType.OGRNIP.normaliseRegex)) {
+            listOf(
+                ExtractedDocument(
+                    docType = DocType.OGRNIP,
+                    value = onlyDigits,
+                    isValidSetup = true,
+                    isValid = isValid(onlyDigits)
+                )
+            )
+        } else emptyList()
+    }
+
+    private fun isValid(input: String): Boolean {
+        return input.substring(0 until 14).toLong() % 13 % 10 == input[14].digitToInt().toLong()
     }
 }
--- src/main/kotlin/codes/spectrum/conf2022/impl/OgrnParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/impl/OgrnParser.kt
@@ -1,10 +1,26 @@
 package codes.spectrum.conf2022.impl
 
+import codes.spectrum.conf2022.doc_type.DocType
 import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.output.ExtractedDocument
 
 class OgrnParser: IDocParser {
+
     override fun parse(input: String): List<ExtractedDocument> {
-        TODO("Not yet implemented")
+        val onlyDigits = input.filter {it.isDigit() }
+        return if (onlyDigits.matches(DocType.OGRN.normaliseRegex)) {
+            listOf(
+                ExtractedDocument(
+                    docType = DocType.OGRN,
+                    value = onlyDigits,
+                    isValidSetup = true,
+                    isValid = isValid(onlyDigits)
+                )
+            )
+        } else emptyList()
+    }
+
+    private fun isValid(input: String): Boolean {
+        return input.substring(0 until 12).toLong() % 11 % 10 == input[12].digitToInt().toLong()
     }
 }
