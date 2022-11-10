df91a7b23612e0c75fbfddcfea3347bd67ec676e
Gavrilov Vadim <gavrilov.v@avtocod.ru>
2022-11-10 17:57:23 +0500

    Gavrilov

--- src/main/kotlin/codes/spectrum/conf2022/impl/StsParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/impl/StsParser.kt
@@ -1,10 +1,24 @@
 package codes.spectrum.conf2022.impl
 
+import codes.spectrum.conf2022.doc_type.DocType
 import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.output.ExtractedDocument
 
 class StsParser: IDocParser {
     override fun parse(input: String): List<ExtractedDocument> {
-        return emptyList()
+        val prepared = input.replace("""[^\dA-ZА-ЯЁ]""".toRegex(option = RegexOption.IGNORE_CASE), "")
+        val find = """(\d{2}[\dА-ЯA-Z]{2}\d{6})""".toRegex().find(prepared)?.groupValues?.get(1)
+        return if(find != null) {
+            listOf(
+                ExtractedDocument(
+                    docType = DocType.STS,
+                    value = find,
+                    isValidSetup = true,
+                    isValid = find.matches(DocType.STS.normaliseRegex),
+                )
+            )
+        } else {
+            emptyList()
+        }
     }
 }
